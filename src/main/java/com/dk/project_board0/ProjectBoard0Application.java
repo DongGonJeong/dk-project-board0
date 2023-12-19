5f0a67087cf3ed59b3e8e3964e2185b38ab030d8
dk <fbt0159@gmail.com>
2023-12-20 03:15:36 +0900

    #16 - thymeleaf3 decoupled logic 설정
    java code로,,
    thymeleaf3decoupled logic을,,
    사용자 application property로 제어할 수 있도록,,
    설정 추가함

--- src/main/java/com/dk/project_board0/ProjectBoard0Application.java
+++ src/main/java/com/dk/project_board0/ProjectBoard0Application.java
@@ -2,7 +2,10 @@ package com.dk.project_board0;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class ProjectBoard0Application {
 
